504953368150663344a7ea1f50a5caaeb88ee8df
Chetu14321 <chetuchethan87@gmail.com>
2025-08-06 22:32:21 +0530

    build update

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.use(cors({
 app.use(express.static("./client/build"))
 app.use(express.static("./build"))
 
-app.use(express.static(path.join(__dirname, "client/build")));
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
